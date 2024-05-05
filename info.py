c983d36d1c99a791aff41d03b755cbe248aa3887
robincinema <166351016+robincinema@users.noreply.github.com>
2024-05-05 08:32:51 +0600

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://robincinema0:B5K69fV6OoivFjkC@robincinema.3taxe7h.mongodb.net/?retryWrites=true&w=majority&appName=robincinema")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
